445d656e193a318da0e50f71a46bbf81396799dd
XingZiLong <xzl2628655535@163.com>
2020-04-02 17:31:53 +0800

    NEW FEATURES!
    Supports not, and, or, and ERROR! You can debug your UNICK program by a simple yet powerful error message.

---
+++ compiler.js
@@ -0,0 +1,33 @@
+function compiler(){
+	var code = document.getElementsByName("input")[0].value;
+	//commands: + - @not ?and %or ~xor ^xand #output
+	var stack = [];
+	var i = 0;
+	var result = [];
+	while (i < code.length) {
+		switch (code[i]) {
+			case "+":
+				stack.push(true);
+				break;
+			case "-":
+				stack.pop();
+				break;
+			case "?":
+				stack.push(stack.pop() && stack.pop());
+				break;
+			case "@":
+				stack.push(!stack.pop());
+				break;
+			case "%":
+				stack.push(stack.pop() || stack.pop());
+				break;
+			case "#":
+				result.push(stack.pop())
+			default:
+				throw new Error("Unidentified character: \'" + code[i] + "\', at char #" + i);
+				break;
+		}
+		i += 1
+	}
+	document.getElementsByName("output")[0].innerHTML = result;
+}
